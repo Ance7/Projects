2c20170cf4a864bfcb18049ac6babf6cc29f9f05
AnZ <ance.anghelo@gmail.com>
2023-06-09 19:44:22 -0400

    Finish tags component

--- calendar-app/components/Tags.jsx
+++ calendar-app/components/Tags.jsx
@@ -1,5 +1,6 @@
 'use client'
 
+import { set } from 'date-fns';
 import styles from '../app/page.module.css'
 import React from 'react';
 import { useState } from 'react';
@@ -10,6 +11,7 @@ const Tags = () => {
   const [colorTag, setColorTag] = useState(['#00b8d8','#ff7cb3','#00a772'])
   const [newTag, setNewTag] = useState('')
   const [newColorTag, setNewColorTag] = useState('')
+  const [showAddTag, setShowAddTag] = useState(false);
 
   // const addEvent = () => {
   //   return (
@@ -58,22 +60,12 @@ const Tags = () => {
     setNewColorTag(e.target.value)
   }
 
-  const addTag = () => {    
-    return (
-      <div className={styles.addTagContainer}>
-        <h3>Nuevo Tag</h3>
-        <div className={styles.addTagBody}>
-          <div>
-            Nombre: 
-            color:
-          </div>
-          <div>
-            <input type='text' onChange={handleNewTag} />
-            <input type='color' onChange={handleNewColorTag} />
-          </div>
-        </div>
-      </div>
-    )
+  const addTag = () => {
+    setTags([...tags, newTag])
+    setColorTag([...colorTag, newColorTag])
+    setNewTag('')
+    setNewColorTag('')
+    setShowAddTag(false)
   }
 
   return (
@@ -93,13 +85,34 @@ const Tags = () => {
             })
           }
 
-        </div>
-          {
-            tags.length < 5 
-            ? <button className={styles.btnAddTask} onClick={() => addTag()}>+</button>
-            : null
-          }
       </div>
+        {
+          tags.length < 7 
+          ? <button className={styles.btnAddTask} onClick={() => setShowAddTag(true)}>+</button>
+          : null
+        }
+      </div>
+      {showAddTag && (
+        <>
+          <div className={styles.addTagOverlay} onClick={() => setShowAddTag(false)} />
+
+          <div className={styles.addTagContainer}>
+            <h3 style={{fontSize: '1.6rem', textDecoration: 'underline', color: '#D6AD60'}}>Nuevo Tag</h3>
+            <div className={styles.addTagBody}>
+              <div>
+                <span>Nombre:</span>
+                <span>Color:</span>
+              </div>
+
+              <div>
+                <input type='text' onChange={handleNewTag} />
+                <input type='color' onChange={handleNewColorTag} />
+              </div>
+            </div>
+              <button className={styles.addTagBtn} onClick={() => {addTag()}} disabled={!newTag || !newColorTag}>Agregar</button>
+          </div>
+        </>
+      )}
     </div>
   );
 }
